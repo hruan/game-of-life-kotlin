a77ea436c77df8e4739ae4e46c7b844dab5963a8
Hang Ruan <hruan@users.noreply.github.com>
2022-11-13 11:03:09 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "gol-kt"
+
---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,102 @@
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
+
+class MainKtTest : ShouldSpec({
+    context("board") {
+        should("should be able to toString board") {
+            val stillLifeBlockBoard = Board(
+                arrayOf(
+                    arrayOf(0, 0, 0, 0),
+                    arrayOf(0, 1, 1, 0),
+                    arrayOf(0, 1, 1, 0),
+                    arrayOf(0, 0, 0, 0)
+                )
+            )
+
+            stillLifeBlockBoard.toString() shouldBe """
+                ....
+                .OO.
+                .OO.
+                ....
+            """.trimIndent()
+        }
+    }
+
+    context("tick") {
+        should("still life block should remain unchanged") {
+            val stillLifeBlockBoard = Board(
+                arrayOf(
+                    arrayOf(0, 0, 0, 0),
+                    arrayOf(0, 1, 1, 0),
+                    arrayOf(0, 1, 1, 0),
+                    arrayOf(0, 0, 0, 0)
+                )
+            )
+
+            tick(stillLifeBlockBoard) shouldBe stillLifeBlockBoard
+        }
+
+        should("still life tub should remain") {
+            val stillLifeTubBoard = Board(
+                arrayOf(
+                    arrayOf(0, 0, 0, 0, 0),
+                    arrayOf(0, 0, 1, 0, 0),
+                    arrayOf(0, 1, 0, 1, 0),
+                    arrayOf(0, 0, 1, 0, 0),
+                    arrayOf(0, 0, 0, 0, 0)
+                )
+            )
+
+            tick(stillLifeTubBoard) shouldBe stillLifeTubBoard
+        }
+
+        should("blinker states should be swap") {
+            val blinkerBoard = Board(
+                arrayOf(
+                    arrayOf(0, 0, 0, 0, 0),
+                    arrayOf(0, 0, 1, 0, 0),
+                    arrayOf(0, 0, 1, 0, 0),
+                    arrayOf(0, 0, 1, 0, 0),
+                    arrayOf(0, 0, 0, 0, 0),
+                )
+            )
+
+            val blinkerBoard2 = Board(
+                arrayOf(
+                    arrayOf(0, 0, 0, 0, 0),
+                    arrayOf(0, 0, 0, 0, 0),
+                    arrayOf(0, 1, 1, 1, 0),
+                    arrayOf(0, 0, 0, 0, 0),
+                    arrayOf(0, 0, 0, 0, 0),
+                )
+            )
+
+            tick(blinkerBoard) shouldBe blinkerBoard2
+            tick(tick(blinkerBoard)) shouldBe blinkerBoard
+        }
+
+        should("pulsar should transition between its states") {
+            val cells =
+                """.................
+                   .................
+                   ....OOO...OOO....
+                   .................
+                   ..O....O.O....O..
+                   ..O....O.O....O..
+                   ..O....O.O....O..
+                   ....OOO...OOO....
+                   .................
+                   ....OOO...OOO....
+                   ..O....O.O....O..
+                   ..O....O.O....O..
+                   ..O....O.O....O..
+                   .................
+                   ....OOO...OOO....
+                   .................
+                   .................""".trimIndent()
+            val pulsar1 = Board(cells, 17)
+
+            tick(tick(tick(pulsar1))) shouldBe pulsar1
+        }
+    }
+})
